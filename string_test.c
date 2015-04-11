f3f812e5ddffc688b75b479dec3c48cefdbb478f
lucyking <lucyking@users.noreply.github.com>
2015-04-11 15:49:38 +0800

    Create string_test.c

---
+++ string_test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+	char str[100];
+	int i,j,len;
+	j=1;
+	while(scanf("%s",str)!=EOF){
+		len=strlen(str);
+
+		printf("%d>",j++);
+		printf("%d:",len);
+
+		for(i=0;i<len;i++){
+			printf("%c",str[i]);
+		}
+		printf("\n");
+
+
+	}
+
+	return 0;
+}
